a423a7a3a5eb0fcd528b5885dda6ddcbe3a9b89b
Lord Meleneth <meleneth@moxie>
2025-09-04 00:03:05 +0000

    In renders a single frame, 2000 lines for what we did in 6 lines before.  Woo.

--- src/pyspire/py_spire.py
+++ src/pyspire/py_spire.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass, field
-
 from collections import defaultdict
 from typing import Callable, Dict, List, Any
+
+import cairocffi
 
 from .sprite import Sprite
 from .primitives import Size
@@ -11,9 +12,20 @@ class PySpire:
     size: Size
     base_filename: str
     sprites: List[Sprite] = field(default_factory=list)
+    frame_no: int = 0
 
     def add_sprite(self, name):
         new_sprite = Sprite(name=name)
         self.sprites.append(new_sprite)
         return new_sprite
 
+    def render_frame(self):
+        surface = cairocffi.ImageSurface(cairocffi.FORMAT_ARGB32, self.size.width, self.size.height)
+        ctx = cairocffi.Context(surface)
+        for sprite in self.sprites:
+            sprite.render(ctx)
+        surface.write_to_png(self.output_filename())
+        self.frame_no = self.frame_no + 1
+
+    def output_filename(self) -> str:
+        return f"{self.base_filename}_{self.frame_no:06_}.png"
